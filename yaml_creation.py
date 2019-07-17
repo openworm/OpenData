a80c20aee490028d770745a41f236f01eabd81d0
EST09 <esthomas09@hotmail.com>
2019-07-17 10:34:01 +0100

    Update yaml_creation.py
    all yaml files ow have standardised keys

--- yaml_creation.py
+++ yaml_creation.py
@@ -10,8 +10,18 @@ from sickle.iterator import OAIResponseIterator
 sickle = Sickle("https://zenodo.org/oai2d")
 recs = sickle.ListRecords(set='user-open-worm-movement-database', metadataPrefix='oai_dc')
 
+#basic idea compare the keys in yaml to the standardise list of keys that I want to include
+#if they all match brilliant - proceed
+#if they don't match
+#find out which keys are missing in the yaml file compared to the standardised list
+#append <missing_key> : nan
+
+
+#taken from subclasses.py
+standardised_list = ['allele', 'arena', 'base_name', 'chromosome', 'food', 'frames per second', 'gene', 'habituation', 'identifier', 'lab', 'media', 'number of segmented skeletons', 'preview link', 'protocol', 'sex', 'software', 'stage', 'strain', 'strain_description', 'timestamp', 'total time (s)', 'ventral_side', 'video micrometers per pixel', 'who', 'days_of_adulthood', 'worm_id']
+
 def main():
-    zip_file_name = "D:/exporty12.zip"
+    zip_file_name = "D:/exporty13.zip"
     print("Creating archive: {:s}".format(zip_file_name))
     with zipfile.ZipFile(zip_file_name, mode="w") as zf:
         for r in recs:
@@ -33,14 +43,20 @@ def main():
             a = [w.replace('\n\n', '\n') for w in a]
             a = [w.replace('\t', '  ') for w in a]
             b = a[0]
+            included_standard_elements = sorted([element for element in standardised_list if (element in b)])
+            unincluded_standard_elements = list(set(standardised_list).difference(included_standard_elements))
+            for i in unincluded_standard_elements:
+                addel = "\n{}".format(i) + " : nan"
+                b = b + addel
             g = b.split('\n', 1)[1]
             c = var3 + g
             img_name = "yamlfile_{}.yaml".format(y)
             print("  Writing image {:s} in the archive".format(img_name))
             zf.writestr(img_name, c)
 
-
 if __name__ == "__main__":
     print("Python {:s} on {:s}\n".format(sys.version, sys.platform))
     main()
     print("Done.")
+
+
