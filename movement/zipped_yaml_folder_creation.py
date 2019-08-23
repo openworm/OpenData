cd58844012b50622963cea94094af220241b05ac
EST09 <EST09@users.noreply.github.com>
2019-08-23 11:48:02 +0100

    Update zipped_yaml_folder_creation.py

--- movement/zipped_yaml_folder_creation.py
+++ movement/zipped_yaml_folder_creation.py
@@ -18,10 +18,10 @@ recs = sickle.ListRecords(set='user-open-worm-movement-database', metadataPrefix
 
 
 #taken from subclasses.py
-standardised_list = ['allele', 'arena', 'base_name', 'chromosome', 'food', 'frames per second', 'gene', 'habituation', 'identifier', 'lab', 'media', 'number of segmented skeletons', 'preview link', 'protocol', 'sex', 'software', 'stage', 'strain', 'strain_description', 'timestamp', 'total time (s)', 'ventral_side', 'video micrometers per pixel', 'who', 'days_of_adulthood', 'worm_id']
+standardised_list = ['allele', 'arena', 'base_name', 'chromosome', 'food', 'frames per second', 'gene', 'habituation', 'lab', 'media', 'number of segmented skeletons', 'preview link', 'protocol', 'sex', 'software', 'stage', 'strain', 'strain_description', 'timestamp', 'total time (s)', 'ventral_side', 'video micrometers per pixel', 'who', 'days_of_adulthood', 'worm_id']
 
 def main():
-    zip_file_name = "D:/exporty13.zip"
+    zip_file_name = "C:/Users/estho/PycharmProjects/GSOC3/movementmetadata_yaml4.zip"
     print("Creating archive: {:s}".format(zip_file_name))
     with zipfile.ZipFile(zip_file_name, mode="w") as zf:
         for r in recs:
@@ -39,6 +39,7 @@ def main():
             a = [w.replace('AX994: daf-22(m130)II.', 'AX994 daf22(m130)II.') for w in a]
             a = [w.replace('OMG2: mIs12[myo-2p::GFP]II; npr-1(ad609)X. OMG19: rmIs349[myo3p::RFP]; npr-1(ad609)X.', 'OMG2 mIs12[myo2pGFP]II npr1(ad609)X. OMG19 rmIs349[myo3pRFP] npr1(ad609)X.') for w in a]
             a = [w.replace('OMG10: mIs12[myo-2p::GFP]II. OMG24: rmIs349[myo3p::RFP].', 'OMG10 mIs12[myo2pGFP]II. OMG24 rmIs349[myo3pRFP].') for w in a]
+            a = [w.replace('-N/A-','None') for w in a]
             a = [w.replace('\n\t', '\n') for w in a]
             a = [w.replace('\n\n', '\n') for w in a]
             a = [w.replace('\t', '  ') for w in a]
@@ -46,7 +47,7 @@ def main():
             included_standard_elements = sorted([element for element in standardised_list if (element in b)])
             unincluded_standard_elements = list(set(standardised_list).difference(included_standard_elements))
             for i in unincluded_standard_elements:
-                addel = "\n{}".format(i) + " : nan"
+                addel = "\n{}".format(i) + " : None"
                 b = b + addel
             g = b.split('\n', 1)[1]
             c = var3 + g
@@ -58,4 +59,3 @@ if __name__ == "__main__":
     print("Python {:s} on {:s}\n".format(sys.version, sys.platform))
     main()
     print("Done.")
-
